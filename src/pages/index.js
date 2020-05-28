2e586759c37090057826b0dcd4ffa9fa3efa2cd3
Chris Atoki <christopher.atoki@yahoo.com>
2020-05-27 20:27:36 -0400

    Update env vars

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,5 @@ import React from "react"
 import Layout from "../components/Layout"
 
 export default function Home() {
-  return (
-    <Layout>
-      Hello world!
-    </Layout>
-  )
+  return <Layout>Hello world!</Layout>
 }

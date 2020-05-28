325e8cbbee91a70c358b8ff458f5124cf780c034
Chris Atoki <christopher.atoki@yahoo.com>
2020-05-28 00:42:48 -0400

    Add GitHub description to the Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Grid } from "@material-ui/core"
+import { Grid, Typography } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
 import Layout from "../components/Layout"
 import RepoCard from "../components/RepoCard"
@@ -12,19 +12,32 @@ const useStyles = makeStyles(() => ({
     width: 500,
     height: 450,
   },
+  description: {
+    color: "darkgrey",
+    paddingTop: 20,
+  },
 }))
 
 const Home = ({
   data: {
     github: {
-      organization: { repositories: { nodes: repos } = {} },
+      organization: { description, repositories: { nodes: repos } = {} },
     },
   },
 }) => {
   const classes = useStyles()
   return (
     <Layout>
-      <Grid container className={classes.root} spacing={2}>
+      <Typography
+        variant="h4"
+        className={classes.description}
+        gutterBottom
+        paragraph
+        align="center"
+      >
+        <i>{description}</i>
+      </Typography>
+      <Grid container className={classes.root} spacing={3} align="center">
         {repos.map(repo => (
           <Grid item xs={12} sm={6} md={4} key={repo.url}>
             <RepoCard repo={repo} key={repo.url} />
@@ -41,6 +54,7 @@ export const query = graphql`
   {
     github {
       organization(login: "debtcollective") {
+        description
         repositories(first: 10) {
           nodes {
             description
